4cf982f4e196fc1846a383fd14b95fdbe06eb851
OswaldCardoz <145090621+OswaldCardoz@users.noreply.github.com>
2024-01-03 10:56:07 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.get('/product/:productId', (req, res) => {
   //Write a code here for endpoint /product/:productId and also print parameter in json form
+   const userId=req.params.productId;
+  res.status(200).json({productId});
 });
 
 module.exports = app;
